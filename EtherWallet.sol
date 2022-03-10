752d9d41babc89ff556476dc2e4b5d89482542ab
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2022-03-10 19:48:16 +0530

    Add files via upload

---
+++ EtherWallet.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint _amount) external {
+        require(msg.sender == owner, "Invalid Owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+}
